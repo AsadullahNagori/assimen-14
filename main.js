d8101ede47cce5dfb47788c1f13583eedd593d89
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-05-20 16:10:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let Guest_List = ["Asad", "Ahmed", "Aliyan"];
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log(`Dear ${Guest_List[i]} :\n you are invited to dinner!\n`);
+}
+let Name = "Asadullah";
+for (let k = 7; k < 17; k++) {
+    console.log(Name);
+}
